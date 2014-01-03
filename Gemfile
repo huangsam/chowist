0f74f7ed8bbf05fd48b62ef1560f772934b97849
Samuel Huang <samhua@cisco.com>
2014-01-02 16:27:20 -0800

    here ya go

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 ruby "1.9.3"
 gem 'sinatra', '1.1.0'
+gem 'mongo'
--- web.rb
+++ web.rb
@@ -1,5 +1,22 @@
 require 'sinatra'
+require 'mongo'
+require 'uri'
+
+def get_connection
+    return @db_connection if @db_connection
+    db = URI.parse(ENV['MONGOHQ_URL'])
+    db_name = db.path.gsub(/^\//, '')
+    @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+    @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+    @db_connection
+end
+ 
+db = get_connection
+
+puts "Collections"
+puts "==========="
+collections = db.collection_names
 
 get '/' do
-  "Hello, world"
+	collections
 end
