14c259e9862df502251453712580651bfe005b18
Samuel Huang <samhua@cisco.com>
2014-01-03 10:50:44 -0800

    more RESTful

--- web.rb
+++ web.rb
@@ -1,6 +1,24 @@
 require 'sinatra'
+require 'json'
+
+str = File.read('public/restaurants.min.json')
+json = JSON.parse(str)
 
 get '/' do
 	content_type :json
-	File.read('public/restaurants.min.json')
+	json.to_json()
 end
+
+get '/:name' do
+	content_type :json
+	output = ''
+	json["places"].map do |key|
+		if key["_id"] == params[:name]
+			output = key.to_json()
+			break
+		end
+	end
+	output
+end
+
+__END__
