11ad95292dc689e961c5cc5e137b77284b7e4cfb
Samuel Huang <samhua@cisco.com>
2014-01-02 22:30:51 -0800

    hiya

--- web.rb
+++ web.rb
@@ -1,28 +1,14 @@
 require 'sinatra'
-require 'mongo'
-require 'uri'
-
-def get_connection
-    return @db_connection if @db_connection
-    db = URI.parse(ENV['MONGOHQ_URL'])
-    db_name = db.path.gsub(/^\//, '')
-    @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
-    @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
-    @db_connection
-end
- 
-db = get_connection
-
-collections = db.collection_names
-
-last_collection = collections[-1]
-coll = db.collection(last_collection)
-
-# just show 5
-darray = []
-docs = coll.find().limit(5)
-docs.each{ |doc| darray += doc.to_json }
 
 get '/' do
-	darray
+	"Hi!"
+end
+
+get '/:name' do
+	name = params[:name]
+	"Hi there #{name}!"
+end
+
+get '/discount' do
+	markdown :intro
 end
