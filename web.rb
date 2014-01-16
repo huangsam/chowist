13f4de592e39764b993e21fdb7470cc76f2e0b19
Samuel Huang <samhuang91@gmail.com>
2014-01-16 15:19:19 -0800

    Update web.rb
    re-indent

--- web.rb
+++ web.rb
@@ -5,12 +5,12 @@ require 'sinatra'
 require 'uri'
 
 def get_connection
-	return @db_connection if @db_connection
-	db = URI.parse(ENV['MONGOHQ_URL'])
-	db_name = db.path.gsub(/^\//, '')
-	@db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
-	@db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
-	@db_connection
+    return @db_connection if @db_connection
+    db = URI.parse(ENV['MONGOHQ_URL'])
+    db_name = db.path.gsub(/^\//, '')
+    @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+    @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+    @db_connection
 end
 
 get '/' do
@@ -22,15 +22,15 @@ get '/places' do
     db = get_connection
     coll = db.collection("places")
     cursor = coll.find()
-	JSON.pretty_generate(cursor.to_a)
+    JSON.pretty_generate(cursor.to_a)
 end
 
 get '/places/:time' do
     content_type :json
     db = get_connection
     coll = db.collection("places")
-	cursor = coll.find("minutes" => {"$lte" => params[:time].to_i})
-	JSON.pretty_generate(cursor.to_a)
+    cursor = coll.find("minutes" => {"$lte" => params[:time].to_i})
+    JSON.pretty_generate(cursor.to_a)
 end
 
 __END__
