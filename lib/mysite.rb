94eb5840bb1b362652b587d74326df9e3b899c0f
Samuel Huang <samhuang91@gmail.com>
2014-01-18 13:34:28 -0800

    forgot one more thing...

--- lib/mysite.rb
+++ lib/mysite.rb
@@ -5,6 +5,17 @@ require 'newrelic_rpm'
 require 'sinatra/base'
 require 'uri'
 
+include Mongo
+
+def get_connection
+    return @db_connection if @db_connection
+    db = URI.parse(ENV['MONGOHQ_URL'])
+    db_name = db.path.gsub(/^\//, '')
+    @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+    @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+    @db_connection
+end
+
 module Website
   class MySite < Sinatra::Base
 
