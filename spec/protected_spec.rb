30a32a09f3952807d4acf950dd5fedaa956cd546
mchamour <mchamour@cisco.com>
2014-01-21 23:16:32 -0800

    added test cases

--- spec/protected_spec.rb
+++ spec/protected_spec.rb
@@ -7,6 +7,11 @@ describe Website::Protected do
     end
     
     describe "get /api/places" do
+        it "should work with valid credentials" do
+            digest_authorize 'cisco', 'chef2'
+            get '/places'
+            expect(last_response).to be_ok
+        end
 
         it "should not work with invalid credentials" do
             digest_authorize 'admin', 'admin'
@@ -20,6 +25,12 @@ describe Website::Protected do
             digest_authorize 'cisco', 'chef2'
             post '/place', {:name => "Candy"}.to_json, "content_type" => "application/json"
             expect(last_response.body) == "Object was unsuccessful."
+        end
+
+        it "should fail with invalid credentials" do
+            digest_authorize 'admin', 'admin'
+            post '/place', {:name => "Candy"}.to_json, "content_type" => "application/json"
+            expect(last_response).to_not be_ok
         end
     end
 end
