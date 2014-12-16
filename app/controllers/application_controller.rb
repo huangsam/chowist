69a31390c7dbcaba08c2d8d7a88ec0df246ba790
Samuel Huang <samhuang91@gmail.com>
2014-12-15 23:46:59 -0800

    Add functional tests to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,12 +44,12 @@ class ApplicationController < ActionController::Base
   # creates database URI
   def create_uri
     mongohq_url = ENV['MONGOHQ_URL']
-    mongo_host = ENV['CISCOCHEF_DB_1_PORT_27017_TCP_ADDR'] ||
-      ENV['MONGO_HOST'] ||
-      '127.0.0.1'
-    mongo_port = ENV['CISCOCHEF_DB_1_PORT_27017_TCP_PORT'] ||
-      ENV['MONGO_PORT'] ||
-      '27017'
+    mongo_host = ENV['CISCOCHEF_DB_1_PORT_27017_TCP_ADDR'] \
+      || ENV['MONGO_HOST'] \
+      || '127.0.0.1'
+    mongo_port = ENV['CISCOCHEF_DB_1_PORT_27017_TCP_PORT'] \
+      || ENV['MONGO_PORT'] \
+      || '27017'
     mongohq_url || "mongodb://#{mongo_host}:#{mongo_port}/ciscochef"
   end
 
@@ -57,8 +57,8 @@ class ApplicationController < ActionController::Base
   def create_query(time, min, max, rating, category)
     query = {}
     query['minutes'] = { '$lte' => time.to_i } unless time.nil?
-    query['maxparty'] = { '$lte' => max.to_i } unless max.nil?
     query['minparty'] = { '$gte' => min.to_i } unless min.nil?
+    query['maxparty'] = { '$lte' => max.to_i } unless max.nil?
     query['rating'] = { '$gte' => rating.to_f } unless rating.nil?
     query['categories'] = { '$in' => [category] } unless category.nil?
     query
--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -16,4 +16,21 @@ describe ApplicationController do
     expect(page).not_to have_title('Cisco Chef')
   end
 
+  it 'should create a mongodb uri' do
+    controller = ApplicationController.new
+    uri = controller.create_uri
+    expect(uri).to match(%r{mongodb://})
+  end
+
+  it 'should create empty query with all nils' do
+    controller = ApplicationController.new
+    query = controller.create_query(nil, nil, nil, nil, nil)
+    expect(query).to eq({})
+  end
+
+  it 'should not create empty query with no nils' do
+    controller = ApplicationController.new
+    query = controller.create_query('60', '3', '8', '3', 'Thai')
+    expect(query).not_to eq({})
+  end
 end
