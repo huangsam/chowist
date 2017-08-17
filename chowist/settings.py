a573f88c599a3444efa9cd7ecbd5f40b3586f09d
Samuel Huang <samhuang91@gmail.com>
2017-08-16 23:15:05 -0700

    Link api to chowist

--- chowist/settings.py
+++ chowist/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_filters'
+    'django_filters',
+    'api.apps.ApiConfig'
 ]
 
 # DRF configuration
