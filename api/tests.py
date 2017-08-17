0e7af12b8a09ae6828c8edd71289c0c0a8a6d3d5
Samuel Huang <samhuang91@gmail.com>
2017-08-17 00:08:52 -0700

    Harden tests for models

--- api/tests.py
+++ api/tests.py
@@ -1,33 +1,42 @@
 from django.test import TestCase
 
+from api.models import Location
+from api.models import Category
+from api.models import Restaurant
+from api.models import Rating
+
 
 class LocationTestCase(TestCase):
     def setUp(self):
-        pass
+        Location.objects.create()
 
-    def test_location_create(self):
-        pass
+    def test_location_get(self):
+        location = Location.objects.get(pk=1)
+        assert type(location) is Location
 
 
 class CategoryTestCase(TestCase):
     def setUp(self):
-        pass
+        Category.objects.create()
 
-    def test_category_create(self):
-        pass
+    def test_category_get(self):
+        category = Category.objects.get(pk=1)
+        assert type(category) is Category
 
 
 class RestaurantTestCase(TestCase):
     def setUp(self):
-        pass
+        Restaurant.objects.create()
 
-    def test_restaurant_create(self):
-        pass
+    def test_restaurant_get(self):
+        restaurant = Restaurant.objects.get(pk=1)
+        assert type(restaurant) is Restaurant
 
 
 class RatingTestCase(TestCase):
     def setUp(self):
-        pass
+        Rating.objects.create()
 
-    def test_rating_create(self):
-        pass
+    def test_rating_get(self):
+        rating = Rating.objects.get(pk=1)
+        assert type(rating) is Rating
