f5cd5a93d58248f89758355ee3d101aa7b25e579
Samuel Huang <samhuang91@gmail.com>
2017-08-16 23:47:31 -0700

    Create test placeholders

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class LocationTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_location_create(self):
+        pass
+
+
+class CategoryTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_category_create(self):
+        pass
+
+
+class RestaurantTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_restaurant_create(self):
+        pass
+
+
+class RatingTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_rating_create(self):
+        pass
