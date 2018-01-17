9f6e8425e3183550133a45caae4a61724b6bdaf7
Samuel Huang <samhuang91@gmail.com>
2018-01-17 09:46:58 -0800

    Change home to index

--- ratings/urls.py
+++ ratings/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from ratings import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.index, name='index')
 ]
--- ratings/views.py
+++ ratings/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
+def index(request):
     return render(request, template_name='index.html')
