85cf00185983b21cbd024f8c62fdc09f83443a0d
Samuel Huang <samhuang91@gmail.com>
2018-02-05 18:32:02 -0800

    Fix HTML template conflicts

--- portal/views.py
+++ portal/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, template_name='home.html')
+    return render(request, template_name='portal/home.html')
