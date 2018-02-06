1a005cc75f7a767b9ecbc26a3aae44aa350ab88b
Samuel Huang <samhuang91@gmail.com>
2018-02-05 22:51:11 -0800

    Fix collectstatic workflow

--- chowist/settings.py
+++ chowist/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
