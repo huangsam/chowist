91e0f159056d782d3e90cb44360721fa5004f5a2
Samuel Huang <samhuang91@gmail.com>
2018-02-05 20:58:01 -0800

    Change restaurant notes to description

--- ratings/models.py
+++ ratings/models.py
@@ -5,13 +5,13 @@ from django.conf import settings
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, null=True)
     address = models.CharField(max_length=255)
     latitude = models.DecimalField(max_digits=17, decimal_places=15)
     longitude = models.DecimalField(max_digits=18, decimal_places=15)
     min_party = models.IntegerField()
     max_party = models.IntegerField()
     yelp_link = models.CharField(max_length=255)
-    notes = models.CharField(max_length=255)
 
     class Meta:
         db_table = 'restaurant'
