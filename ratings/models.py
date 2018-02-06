fa7b24f1c51c77929cf12cf3621116d4090af1e5
Samuel Huang <samhuang91@gmail.com>
2018-02-05 22:04:46 -0800

    Enforce unique constraints for Restaurant

--- ratings/models.py
+++ ratings/models.py
@@ -6,12 +6,12 @@ from django.conf import settings
 class Restaurant(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255, null=True)
-    address = models.CharField(max_length=255)
+    address = models.CharField(max_length=255, unique=True)
     latitude = models.DecimalField(max_digits=17, decimal_places=15)
     longitude = models.DecimalField(max_digits=18, decimal_places=15)
     min_party = models.IntegerField()
     max_party = models.IntegerField()
-    yelp_link = models.CharField(max_length=255)
+    yelp_link = models.CharField(max_length=255, unique=True)
 
     class Meta:
         db_table = 'restaurant'
