1d63f615ac58cc8c548cdd8e359694355e5b1843
Samuel Huang <samhuang91@gmail.com>
2018-03-19 23:03:17 -0700

    Add BootstrapForm to beautify form_template

--- portal/forms.py
+++ portal/forms.py
@@ -3,7 +3,16 @@ from django import forms
 
 # Create your forms here.
 
-class UserForm(forms.ModelForm):
+class BootstrapForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+            visible.field.widget.attrs['placeholder'] = 'Please enter value'
+
+
+class UserForm(BootstrapForm):
     password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
