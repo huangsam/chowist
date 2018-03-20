f277c0194ba078dc146b1386465d9ce3c7d1683c
Samuel Huang <samhuang91@gmail.com>
2018-03-19 19:17:02 -0700

    Add DetailView links to ListView

--- places/urls.py
+++ places/urls.py
@@ -7,5 +7,5 @@ from places import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('restaurants/', views.RestaurantListView.as_view(), name='restaurant-list'),
-    path('restaurants/<int:pk>', views.RestaurantDetailView.as_view(), name='restaurant-detail')
+    path('restaurants/<int:pk>', views.RestaurantDetailView.as_view(), name='restaurant-detail'),
 ]
