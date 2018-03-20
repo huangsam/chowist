457958e9044941a3166c19dcbd86139a8d5743a9
Samuel Huang <samhuang91@gmail.com>
2018-03-20 09:20:54 -0700

    Handle sub-path and root-path

--- portal/templatetags/base_extras.py
+++ portal/templatetags/base_extras.py
@@ -5,6 +5,10 @@ register = template.Library()
 
 @register.simple_tag
 def nav_active(request, url):
-    if request.path == reverse(url):
-        return 'active'
+    current_url = request.path
+    reverse_url = reverse(url)
+    if reverse_url in current_url:
+        # Handle sub-path and root-path
+        if reverse_url != '/' or current_url == '/':
+            return 'active'
     return ''
