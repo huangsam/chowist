5a042951196e6c7715450e8d29304f37ce2844ec
Samuel Huang <samhuang91@gmail.com>
2018-03-20 21:12:07 -0700

    Add UserForm test cases

--- portal/tests.py
+++ portal/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from portal.forms import UserForm
 
 # Create your tests here.
+
+class UserFormTestCase(TestCase):
+    """UserForm test suite"""
+
+    def test_userform_invalid(self):
+        form = UserForm()
+        self.assertEquals(form.is_valid(), False)
+
+    def test_userform_valid(self):
+        form_data = {
+            'username': 'johndoe',
+            'email': 'johndoe@example.org',
+            'password': 's3cret123'
+        }
+        form = UserForm(form_data)
+        self.assertEquals(form.is_valid(), True)
