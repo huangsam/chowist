b8b7e20191952835acc0aefd4fd059892dec21ee
Samuel Huang <samhuang91@gmail.com>
2018-03-21 00:22:13 -0700

    Add test for RestaurantListView

--- places/tests/test_views.py
+++ places/tests/test_views.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from places.models import Restaurant
+
 # Create your tests here.
 
 class HomeViewTestCase(TestCase):
@@ -21,6 +23,16 @@ class RestaurantListViewTestCase(TestCase):
     """RestaurantListView test suite"""
 
     expected_url = '/places/restaurants/'
+    restaurant_count = 25
+
+    @classmethod
+    def setUpTestData(cls):
+        for i in range(cls.restaurant_count):
+            Restaurant.objects.create(
+                name='Five Guys {rid}'.format(rid=i), address='Bogus {rid}'.format(rid=i),
+                latitude=0.00, longitude=0.00,
+                min_party=1, max_party=6,
+                yelp_link='/five-guys-earth-{rid}'.format(rid=i))
 
     def test_desired_location(self):
         resp = self.client.get(self.expected_url)
@@ -29,3 +41,8 @@ class RestaurantListViewTestCase(TestCase):
     def test_desired_name(self):
         reverse_url = reverse('places:restaurant-list')
         self.assertEquals(reverse_url, self.expected_url)
+
+    def test_desired_data(self):
+        resp = self.client.get(self.expected_url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(len(resp.context['restaurant_list']), self.restaurant_count)
