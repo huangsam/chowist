e01f7c39f647b59edc97464cb5f8a7878141ec02
Samuel Huang <samhuang91@gmail.com>
2018-04-06 12:39:19 -0700

    Test authentication for profile

--- portal/tests/test_views.py
+++ portal/tests/test_views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 
@@ -69,7 +70,18 @@ class ProfileFormViewTestCase(TestCase):
     desired_url = '/profile/'
     desired_name = 'portal:profile'
 
+    def setUp(self):
+        User.objects.create_user('john', 'john@example.org', 'secret123')
+
     def test_desired_location(self):
+        self.client.login(username='john', password='secret123')
+        resp = self.client.get(self.desired_url)
+        self.assertEqual(resp.status_code, 200)
+        self.client.logout()
+        resp = self.client.get(self.desired_url)
+        self.assertEqual(resp.status_code, 302)
+
+    def test_desired_location_redirect(self):
         resp = self.client.get(self.desired_url)
         self.assertEqual(resp.status_code, 302)
 
