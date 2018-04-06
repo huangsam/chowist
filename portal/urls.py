7485ba4fe538633a91a2ebf67f15af9d29706791
Samuel Huang <samhuang91@gmail.com>
2018-04-06 12:26:40 -0700

    Add additional accounts URLs

--- portal/urls.py
+++ portal/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 from portal import views
@@ -7,8 +7,7 @@ from portal import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('signup/', views.UserFormView.as_view(), name='signup'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
 ]
