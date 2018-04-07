614ff7787f89aa61e11e6674ad34ba9bab0ed333
Samuel Huang <samhuang91@gmail.com>
2018-04-07 00:25:36 -0700

    Fix password change view

--- portal/urls.py
+++ portal/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from django.contrib.auth import views as auth_views
 
 from portal import views
@@ -7,7 +7,10 @@ from portal import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(success_url='done'), name='password_change'),
+    path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('signup/', views.UserFormView.as_view(), name='signup'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
 ]
