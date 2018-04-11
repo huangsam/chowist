d41831d319564f6bbc966665ed48b9a5caa2f168
Samuel Huang <samhuang91@gmail.com>
2018-04-11 08:30:42 -0700

    Treat settings as base config

--- chowist/wsgi.py
+++ chowist/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chowist.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chowist.settings.base")
 
 application = get_wsgi_application()
