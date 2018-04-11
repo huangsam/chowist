f8273f59983c20dc3b08504b111adbc7c3210ce0
Samuel Huang <samhuang91@gmail.com>
2018-04-10 22:09:10 -0700

    Add RestaurantUpdateView

--- places/urls.py
+++ places/urls.py
@@ -7,5 +7,6 @@ from places import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('restaurants/', views.RestaurantListView.as_view(), name='restaurant-list'),
-    path('restaurants/<int:pk>', views.RestaurantDetailView.as_view(), name='restaurant-detail'),
+    path('restaurants/<int:pk>/', views.RestaurantDetailView.as_view(), name='restaurant-detail'),
+    path('restaurants/<int:pk>/update/', views.RestaurantUpdateView.as_view(), name='restaurant-update'),
 ]
--- places/views.py
+++ places/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView
 
 from places.models import Restaurant
 
@@ -19,3 +20,9 @@ class RestaurantListView(ListView):
 class RestaurantDetailView(DetailView):
     model = Restaurant
     context_object_name = 'restaurant'
+
+
+class RestaurantUpdateView(UpdateView):
+    model = Restaurant
+    fields = ['name', 'description', 'address', 'min_party', 'max_party', 'yelp_link']
+    template_name_suffix = '_update'
