e8b55f350cbedfe4fc2de1979c89ef73bdf3e21b
Samuel Huang <samhuang91@gmail.com>
2018-04-11 08:36:45 -0700

    Fix base directory

--- chowist/settings/base.py
+++ chowist/settings/base.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+CUR_PATH = os.path.abspath(__file__)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(CUR_PATH)))
 
 
 # Quick-start development settings - unsuitable for production
