f841c70356b45ca8b95a2004d641784fdac77b2e
Samuel Huang <samhuang91@gmail.com>
2018-04-10 22:30:08 -0700

    Use generic FormView

--- portal/views.py
+++ portal/views.py
@@ -2,7 +2,7 @@ import logging
 
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
-from django.contrib.auth import authenticate, login
+from django.views.generic.edit import FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import View
 
@@ -17,35 +17,19 @@ class HomeView(TemplateView):
     template_name = 'portal/home.html'
 
 
-class UserFormView(View):
+class UserFormView(FormView):
     form_class = UserForm
     template_name = 'portal/signup.html'
+    success_url = '/'
 
-    def get(self, request):
-        form = self.form_class(None)
-        return render(request, self.template_name, {'form': form})
-
-    def post(self, request):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user.set_password(password)
-            user.save()
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                if user.is_active:
-                    logger.info('User {usr} registered to the system'.format(usr=username))
-                    login(request, user)
-                    return redirect('portal:home')
-                else:
-                    logger.warn('User {usr} is inactive'.format(usr=username))
-            else:
-                logger.warn('User {usr} was not created properly'.format(usr=username))
-        else:
-            logger.warn('Invalid signup form data')
-        return render(request, self.template_name, {'form': form})
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user.set_password(password)
+        user.save()
+        logger.info('User {usr} registered to the system'.format(usr=username))
+        return super().form_valid(form)
 
 
 class ProfileView(LoginRequiredMixin, View):
