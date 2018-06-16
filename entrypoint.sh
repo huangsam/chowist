a6c0eee3f142c6f51f772b80625c57eac47c4e61
Samuel Huang <samhuang91@gmail.com>
2018-06-16 10:56:31 -0700

    Add docs to scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+# Apply db migrations against database
 python manage.py migrate
+
+# Load test data for restaurants and users
 python manage.py loaddata restaurant user
+
+# Start the development server
 python manage.py runserver 0.0.0.0:8000
