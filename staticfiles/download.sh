fe991983fed7f0ff1d519b3661c2c9c713e27998
Samuel Huang <samhuang91@gmail.com>
2018-07-28 10:51:06 -0700

    Update bootstrap to v4.1.3

--- staticfiles/download.sh
+++ staticfiles/download.sh
@@ -5,7 +5,7 @@ JQUERY='jquery-3.3.1.slim.min.js'
 curl -L "https://code.jquery.com/$JQUERY" -o "./js/$JQUERY"
 
 # Download Bootstrap CSS and JS
-BS_VERSION='4.1.1'
+BS_VERSION='4.1.3'
 BS_PREFIX='bootstrap.min'
 curl -L "https://stackpath.bootstrapcdn.com/bootstrap/$BS_VERSION/js/$BS_PREFIX.js" -o "./js/$BS_PREFIX.js"
 curl -L "https://stackpath.bootstrapcdn.com/bootstrap/$BS_VERSION/css/$BS_PREFIX.css" -o "./css/$BS_PREFIX.css"
