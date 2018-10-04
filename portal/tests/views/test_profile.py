c4c12f99f93d582db627d875b2d4bd7c07f23f44
Samuel Huang <samhuang91@gmail.com>
2018-10-03 21:41:23 -0700

    Move portal tests into proper place

--- portal/tests/views/test_profile.py
+++ portal/tests/views/test_profile.py
@@ -3,36 +3,6 @@ from django.test import TestCase
 from django.urls import reverse
 
 
-class TestHomeView(TestCase):
-    """HomeView test suite"""
-
-    desired_url = '/'
-    desired_name = 'portal:home'
-
-    def test_desired_location(self):
-        resp = self.client.get(self.desired_url)
-        self.assertEqual(resp.status_code, 200)
-
-    def test_desired_name(self):
-        reverse_url = reverse(self.desired_name)
-        self.assertEquals(reverse_url, self.desired_url)
-
-
-class TestUserFormView(TestCase):
-    """UserFormView test suite"""
-
-    desired_url = '/signup/'
-    desired_name = 'portal:signup'
-
-    def test_desired_location(self):
-        resp = self.client.get(self.desired_url)
-        self.assertEqual(resp.status_code, 200)
-
-    def test_desired_name(self):
-        reverse_url = reverse(self.desired_name)
-        self.assertEquals(reverse_url, self.desired_url)
-
-
 class TestProfileFormView(TestCase):
     """ProfileFormView test suite"""
 
---
+++ portal/tests/views/test_user.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
+
+
+class TestUserFormView(TestCase):
+    """UserFormView test suite"""
+
+    desired_url = '/signup/'
+    desired_name = 'portal:signup'
+
+    def test_desired_location(self):
+        resp = self.client.get(self.desired_url)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_desired_name(self):
+        reverse_url = reverse(self.desired_name)
+        self.assertEquals(reverse_url, self.desired_url)
