fe73ef719724f2a8bf10529d2e6ec59a7e09fda7
Samuel Huang <samhuang91@gmail.com>
2018-10-03 21:39:34 -0700

    Move tests into views package

---
+++ places/tests/views/test_home.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class TestHomeView(TestCase):
+    """HomeView test suite"""
+
+    desired_url = '/places/'
+    desired_name = 'places:home'
+
+    def test_desired_location(self):
+        resp = self.client.get(self.desired_url)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_desired_name(self):
+        reverse_url = reverse(self.desired_name)
+        self.assertEquals(reverse_url, self.desired_url)
--- places/tests/views/test_restaurant.py
+++ places/tests/views/test_restaurant.py
@@ -5,21 +5,6 @@ from django.urls import reverse
 from places.models import Restaurant
 
 
-class TestHomeView(TestCase):
-    """HomeView test suite"""
-
-    desired_url = '/places/'
-    desired_name = 'places:home'
-
-    def test_desired_location(self):
-        resp = self.client.get(self.desired_url)
-        self.assertEqual(resp.status_code, 200)
-
-    def test_desired_name(self):
-        reverse_url = reverse(self.desired_name)
-        self.assertEquals(reverse_url, self.desired_url)
-
-
 class TestRestaurantListView(TestCase):
     """RestaurantListView test suite"""
 
