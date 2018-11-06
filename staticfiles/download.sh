ae04a1bfb537000fe1e091d55d6929638b9afe4f
Samuel Huang <samhuang91@gmail.com>
2018-11-06 08:13:07 -0800

    Update popper to 1.14.4

--- staticfiles/download.sh
+++ staticfiles/download.sh
@@ -11,6 +11,6 @@ curl -L "https://stackpath.bootstrapcdn.com/bootstrap/$BS_VERSION/js/$BS_PREFIX.
 curl -L "https://stackpath.bootstrapcdn.com/bootstrap/$BS_VERSION/css/$BS_PREFIX.css" -o "./css/$BS_PREFIX.css"
 
 # Download Popper.js
-POPPER_VERSION='1.14.3'
+POPPER_VERSION='1.14.4'
 POPPER='popper.min.js'
 curl -L "https://cdnjs.cloudflare.com/ajax/libs/popper.js/$POPPER_VERSION/umd/$POPPER" -o "./js/$POPPER"
