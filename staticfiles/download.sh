106c5fdaa298acfa783c1f4468e5b21d01b3235f
Samuel Huang <samhuang91@gmail.com>
2019-03-07 08:39:23 -0800

    Update bootstrap dependencies

--- staticfiles/download.sh
+++ staticfiles/download.sh
@@ -5,12 +5,12 @@ JQUERY='jquery-3.3.1.slim.min.js'
 curl -L "https://code.jquery.com/$JQUERY" -o "./js/$JQUERY"
 
 # Download Bootstrap CSS and JS
-BS_VERSION='4.1.3'
+BS_VERSION='4.3.1'
 BS_PREFIX='bootstrap.min'
 curl -L "https://stackpath.bootstrapcdn.com/bootstrap/$BS_VERSION/js/$BS_PREFIX.js" -o "./js/$BS_PREFIX.js"
 curl -L "https://stackpath.bootstrapcdn.com/bootstrap/$BS_VERSION/css/$BS_PREFIX.css" -o "./css/$BS_PREFIX.css"
 
 # Download Popper.js
-POPPER_VERSION='1.14.4'
+POPPER_VERSION='1.14.7'
 POPPER='popper.min.js'
 curl -L "https://cdnjs.cloudflare.com/ajax/libs/popper.js/$POPPER_VERSION/umd/$POPPER" -o "./js/$POPPER"
