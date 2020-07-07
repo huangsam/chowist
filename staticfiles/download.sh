92b09b73fc2f193ef34b224cc5dfac049774f1d1
Samuel Huang <samhuang91@gmail.com>
2020-07-07 14:16:10 -0700

    Update bootstrap to 4.5.0

--- staticfiles/download.sh
+++ staticfiles/download.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Download jQuery
-JQUERY='jquery-3.4.1.slim.min.js'
+JQUERY='jquery-3.5.1.slim.min.js'
 curl -L "https://code.jquery.com/$JQUERY" -o "./js/$JQUERY"
 
 # Download Bootstrap CSS and JS
-BS_VERSION='4.4.1'
+BS_VERSION='4.5.0'
 BS_PREFIX='bootstrap.min'
 curl -L "https://stackpath.bootstrapcdn.com/bootstrap/$BS_VERSION/js/$BS_PREFIX.js" -o "./js/$BS_PREFIX.js"
 curl -L "https://stackpath.bootstrapcdn.com/bootstrap/$BS_VERSION/css/$BS_PREFIX.css" -o "./css/$BS_PREFIX.css"
