0245666dad458ed8a5b19f1eb4ee81b609572981
Samuel Huang <samhuang91@gmail.com>
2020-07-09 20:43:10 -0700

    Use AUTH_USER_MODEL

--- portal/models.py
+++ portal/models.py
@@ -1,11 +1,13 @@
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile"
+    )
     bio = models.TextField(max_length=500, blank=True)
     address = models.CharField(max_length=255, blank=True)
     birth_date = models.DateField(null=True)
@@ -20,12 +22,12 @@ class Profile(models.Model):
         return f"Profile for user {self.user.username} w/ email: {self.user.email or 'N/A'}"
 
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
