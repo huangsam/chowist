150391bcd41e7de996ae37d066040168229add28
Samuel Huang <samhuang91@gmail.com>
2020-07-11 19:31:02 -0700

    Add tests for ReviewForm

---
+++ places/tests/forms/test_review.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+
+from places.forms import ReviewForm
+
+
+class TestReviewForm(TestCase):
+    """ReviewForm test suite"""
+
+    def test_reviewform_invalid(self):
+        form = ReviewForm()
+        self.assertFalse(form.is_valid())
+
+    def test_reviewform_valid(self):
+        form_data = {
+            "title": "Great restaurant",
+            "body": "This restaurant is really good",
+            "rating": 5,
+        }
+        form = ReviewForm(form_data)
+        self.assertTrue(form.is_valid())
