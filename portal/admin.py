6bfeb3768547afdba3f47011edbcfc4cad1a5518
Samuel Huang <samhuang91@gmail.com>
2020-08-12 08:49:38 -0700

    Customize admin pages for portal app

--- portal/admin.py
+++ portal/admin.py
@@ -2,4 +2,17 @@ from django.contrib import admin
 
 from portal.models import Profile
 
-admin.site.register(Profile)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["username", "email_address", "birth_date"]
+
+    @staticmethod
+    def username(obj):
+        return obj.user.username
+
+    @staticmethod
+    def email_address(obj):
+        return obj.user.email
+
+
+admin.site.register(Profile, ProfileAdmin)
