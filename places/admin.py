eea04e126f85c196d2924a438e8bde1e1277225d
Samuel Huang <samhuang91@gmail.com>
2020-08-12 08:43:38 -0700

    Customize admin pages for places app

--- places/admin.py
+++ places/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 
 from places.models import Category, Restaurant, Review
 
-admin.site.register(Category)
-admin.site.register(Restaurant)
-admin.site.register(Review)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ["name", "address", "description"]
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ["rating", "author", "place"]
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Restaurant, RestaurantAdmin)
+admin.site.register(Review, ReviewAdmin)
