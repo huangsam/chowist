46dddcb597a8612160c1f479a2c9b57c1979a3b3
Samuel Huang <samhuang91@gmail.com>
2020-08-28 23:39:19 -0700

    Refactor nav_active logic

--- portal/templatetags/base_extras.py
+++ portal/templatetags/base_extras.py
@@ -3,13 +3,17 @@ from django.urls import reverse
 
 register = template.Library()
 
+# Module-level constants
+_IS_ACTIVE = "active"
+_IS_INACTIVE = ""
+
 
 @register.simple_tag
 def nav_active(request, url):
     current_url = request.path
     reverse_url = reverse(url)
-    if reverse_url in current_url:
-        # Handle sub-path and root-path
-        if reverse_url != "/" or current_url == "/":
-            return "active"
-    return ""
+    if reverse_url == current_url:
+        return _IS_ACTIVE
+    if reverse_url in current_url and reverse_url != "/":
+        return _IS_ACTIVE
+    return _IS_INACTIVE
