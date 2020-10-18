ab0f9475e3d45c32b212befde726fc0b6a7bfc6d
Samuel Huang <samhuang91@gmail.com>
2020-10-17 21:30:00 -0700

    Update staticfile dependencies

--- staticfiles/download.sh
+++ staticfiles/download.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+CLOUDFLARE_BASE="https://cdnjs.cloudflare.com/ajax/libs"
+
 # Download jQuery
-JQUERY_VERSION='3.5.1'
-JQUERY="jquery-$JQUERY_VERSION.slim.min.js"
-curl -L "https://code.jquery.com/$JQUERY" -o './jquery.slim.min.js'
+JQUERY_VERSION="3.5.1"
+JQUERY_JS="jquery.slim.min.js"
+curl -L "$CLOUDFLARE_BASE/jquery/$JQUERY_VERSION/$JQUERY_JS" -o "./js/$JQUERY_JS"
 
 # Download Bootstrap CSS and JS
-BS_VERSION='4.5.0'
-BS_PREFIX='bootstrap.min'
-curl -L "https://stackpath.bootstrapcdn.com/bootstrap/$BS_VERSION/js/$BS_PREFIX.js" -o "./js/$BS_PREFIX.js"
-curl -L "https://stackpath.bootstrapcdn.com/bootstrap/$BS_VERSION/css/$BS_PREFIX.css" -o "./css/$BS_PREFIX.css"
+BS_VERSION="4.5.3"
+BS_JS="bootstrap.min.js"
+BS_CSS="bootstrap.min.css"
+curl -L "$CLOUDFLARE_BASE/twitter-bootstrap/$BS_VERSION/js/$BS_JS" -o "./js/$BS_JS"
+curl -L "$CLOUDFLARE_BASE/twitter-bootstrap/$BS_VERSION/css/$BS_CSS" -o "./css/$BS_CSS"
 
 # Download Popper.js
-POPPER_VERSION='1.16.0'
-POPPER='popper.min.js'
-curl -L "https://cdnjs.cloudflare.com/ajax/libs/popper.js/$POPPER_VERSION/umd/$POPPER" -o "./js/$POPPER"
+POPPER_VERSION="1.16.1"
+POPPER_JS="popper.min.js"
+curl -L "$CLOUDFLARE_BASE/popper.js/$POPPER_VERSION/umd/$POPPER_JS" -o "./js/$POPPER_JS"
