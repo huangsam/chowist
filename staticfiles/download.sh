8bdcc0df1c9f84eac4332fde0543e388520f459f
Samuel Huang <samhuang91@gmail.com>
2023-02-18 11:37:50 -0800

    Fixup navbar and main content

--- staticfiles/download.sh
+++ staticfiles/download.sh
@@ -12,3 +12,10 @@ curl -L "$CLOUDFLARE_BASE/twitter-bootstrap/$BS_VERSION/js/$BS_JS" -o "./js/$BS_
 curl -L "$CLOUDFLARE_BASE/twitter-bootstrap/$BS_VERSION/js/$BS_JS_MAP" -o "./js/$BS_JS_MAP"
 curl -L "$CLOUDFLARE_BASE/twitter-bootstrap/$BS_VERSION/css/$BS_CSS" -o "./css/$BS_CSS"
 curl -L "$CLOUDFLARE_BASE/twitter-bootstrap/$BS_VERSION/css/$BS_CSS_MAP" -o "./css/$BS_CSS_MAP"
+
+# Download Popper.js
+POPPER_VERSION="2.11.6"
+POPPER_JS="popper.min.js"
+POPPER_JS_MAP="$POPPER_JS.map"
+curl -L "$CLOUDFLARE_BASE/popper.js/$POPPER_VERSION/umd/$POPPER_JS" -o "./js/$POPPER_JS"
+curl -L "$CLOUDFLARE_BASE/popper.js/$POPPER_VERSION/umd/$POPPER_JS_MAP" -o "./js/$POPPER_JS_MAP"
