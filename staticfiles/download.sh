b478d144015c9019b919d345906aaefa112fdb59
Samuel Huang <samhuang91@gmail.com>
2023-02-18 11:18:41 -0800

    Revise staticfile composition

--- staticfiles/download.sh
+++ staticfiles/download.sh
@@ -2,15 +2,8 @@
 
 CLOUDFLARE_BASE="https://cdnjs.cloudflare.com/ajax/libs"
 
-# Download jQuery
-JQUERY_VERSION="3.5.1"
-JQUERY_JS="jquery.slim.min.js"
-JQUERY_JS_MAP="jquery.slim.min.map"
-curl -L "$CLOUDFLARE_BASE/jquery/$JQUERY_VERSION/$JQUERY_JS" -o "./js/$JQUERY_JS"
-curl -L "$CLOUDFLARE_BASE/jquery/$JQUERY_VERSION/$JQUERY_JS_MAP" -o "./js/$JQUERY_JS_MAP"
-
 # Download Bootstrap CSS and JS
-BS_VERSION="4.6.2"
+BS_VERSION="5.2.3"
 BS_JS="bootstrap.min.js"
 BS_JS_MAP="$BS_JS.map"
 BS_CSS="bootstrap.min.css"
@@ -19,10 +12,3 @@ curl -L "$CLOUDFLARE_BASE/twitter-bootstrap/$BS_VERSION/js/$BS_JS" -o "./js/$BS_
 curl -L "$CLOUDFLARE_BASE/twitter-bootstrap/$BS_VERSION/js/$BS_JS_MAP" -o "./js/$BS_JS_MAP"
 curl -L "$CLOUDFLARE_BASE/twitter-bootstrap/$BS_VERSION/css/$BS_CSS" -o "./css/$BS_CSS"
 curl -L "$CLOUDFLARE_BASE/twitter-bootstrap/$BS_VERSION/css/$BS_CSS_MAP" -o "./css/$BS_CSS_MAP"
-
-# Download Popper.js
-POPPER_VERSION="1.16.1"
-POPPER_JS="popper.min.js"
-POPPER_JS_MAP="$POPPER_JS.map"
-curl -L "$CLOUDFLARE_BASE/popper.js/$POPPER_VERSION/umd/$POPPER_JS" -o "./js/$POPPER_JS"
-curl -L "$CLOUDFLARE_BASE/popper.js/$POPPER_VERSION/umd/$POPPER_JS_MAP" -o "./js/$POPPER_JS_MAP"
