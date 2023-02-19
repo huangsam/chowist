a5ada0f5d08916da17fe0db4a3580a627be59ebf
Samuel Huang <samhuang91@gmail.com>
2023-02-18 23:28:03 -0800

    Maintain query params between ListView pages

--- portal/templatetags/base_extras.py
+++ portal/templatetags/base_extras.py
@@ -3,9 +3,10 @@ from django.urls import reverse
 
 register = template.Library()
 
-# Module-level constants
-_IS_ACTIVE = "active"
-_IS_INACTIVE = ""
+
+class _NavigationStatus:
+    ACTIVE = "active"
+    INACTIVE = ""
 
 
 @register.simple_tag
@@ -13,7 +14,17 @@ def nav_active(request, url):
     current_url = request.path
     reverse_url = reverse(url)
     if reverse_url == current_url:
-        return _IS_ACTIVE
+        return _NavigationStatus.ACTIVE
     if reverse_url in current_url and reverse_url != "/":
-        return _IS_ACTIVE
-    return _IS_INACTIVE
+        return _NavigationStatus.ACTIVE
+    return _NavigationStatus.INACTIVE
+
+
+@register.simple_tag(takes_context=True)
+def updated_params(context, **kwargs):
+    # https://blog.ovalerio.net/archives/1512
+    # https://docs.djangoproject.com/en/4.1/ref/request-response/
+    query_dict = context['request'].GET.copy()
+    for k, v in kwargs.items():
+        query_dict[k] = v
+    return query_dict.urlencode()
