51d32c4ce7e5abc0be9e1c3e7b15cfd896a6bb91
Samuel Huang <samhuang91@gmail.com>
2023-07-24 22:40:59 -0700

    Update download.sh
    Update Bootstrap to 5.3.0 and update Popper to 2.11.8

--- staticfiles/download.sh
+++ staticfiles/download.sh
@@ -3,7 +3,7 @@
 CLOUDFLARE_BASE="https://cdnjs.cloudflare.com/ajax/libs"
 
 # Download Bootstrap CSS and JS
-BS_VERSION="5.2.3"
+BS_VERSION="5.3.0"
 BS_JS="bootstrap.min.js"
 BS_JS_MAP="$BS_JS.map"
 BS_CSS="bootstrap.min.css"
@@ -14,7 +14,7 @@ curl -L "$CLOUDFLARE_BASE/twitter-bootstrap/$BS_VERSION/css/$BS_CSS" -o "./css/$
 curl -L "$CLOUDFLARE_BASE/twitter-bootstrap/$BS_VERSION/css/$BS_CSS_MAP" -o "./css/$BS_CSS_MAP"
 
 # Download Popper.js
-POPPER_VERSION="2.11.6"
+POPPER_VERSION="2.11.8"
 POPPER_JS="popper.min.js"
 POPPER_JS_MAP="$POPPER_JS.map"
 curl -L "$CLOUDFLARE_BASE/popper.js/$POPPER_VERSION/umd/$POPPER_JS" -o "./js/$POPPER_JS"
