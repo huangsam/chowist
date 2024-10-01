4529effa750aadfe061e7df6ff8510de9805a43b
Samuel Huang <samhuang91@gmail.com>
2024-09-30 23:34:23 -0700

    Use Postgres in test env

--- chowist/settings/test.py
+++ chowist/settings/test.py
@@ -6,10 +6,10 @@ from chowist.settings.base import *  # noqa
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "circle_test",
+        "NAME": "postgres",
         "HOST": "127.0.0.1",
         "PORT": "5432",
-        "USERNAME": "circleci",
-        "PASSWORD": "circleci",
+        "USERNAME": "postgres",
+        "PASSWORD": "postgres",
     }
 }
